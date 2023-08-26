ce18d0a666eec8edc3145bf45bbfdb9da3a34671
kimaniMbatiaMoringa <kimani.mbatia@student.moringaschool.com>
2023-08-26 13:36:29 +0300

    Assignment Completed

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 class Person:
+    def __init__(self, yes="true"):
+        self.yes = yes
     # Class body goes here
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.c")    
 
     #Instance method definition
     pass
