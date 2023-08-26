95fe916f30506c59e7145a035a569bd72f7b1558
kimaniMbatiaMoringa <kimani.mbatia@student.moringaschool.com>
2023-08-26 13:37:29 +0300

    Assignment completed

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
-
+import ipdb
+print("Running dog class")
 class Dog:
     # Class body goes here
+    def __init__(self,breed="Mutt"):
+        #self.name = name
+        self.breed = breed
 
     #Instance method definition
-    pass
+    def bark(self):
+        print("Woof!")
+
+    def sit(self):
+        print("The dog is sitting.")    
+
+#fido = Dog("fido")
+#ipdb.set_trace()
+
+
